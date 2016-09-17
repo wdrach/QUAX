38fd6cbfbf1c6909cce4156b1441f687c7a5ff6d
Will Drach <will.drach@gmail.com>
2016-09-16 19:48:15 -0600

    hotfixin!

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,7 +11,7 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.html$/, loaders: ["html"]},
-      {test: /\.(gif|svg|ttf|eot)$/, loaders ["file"]}
+      {test: /\.(gif|svg|ttf|eot)$/, loaders: ["file"]}
     ]
   },
 
